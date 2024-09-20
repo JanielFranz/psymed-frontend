e7d229b2ad893fa717bfdd231974955daea71ce7
RominaMaita <u202213765@upc.edu.pe>
2024-09-20 15:34:20 -0500

    chore(patient): added new component for patient create and edit.

---
+++ src/app/user-mamagement/components/patient-create-and-edit/patient-create-and-edit.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-patient-create-and-edit',
+  standalone: true,
+  imports: [],
+  templateUrl: './patient-create-and-edit.component.html',
+  styleUrl: './patient-create-and-edit.component.css'
+})
+export class PatientCreateAndEditComponent {
+
+}
