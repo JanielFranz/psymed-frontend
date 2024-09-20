6ab788fae3aba847286a25ab6db901d5e8cc4691
Ro <u202213765@upc.edu.pe>
2024-09-19 19:52:36 -0500

    feat(clinical-diagnosis): added clinical diagnosis entity to model.

---
+++ src/app/psymed/model/clinical-diagnosis.entity.ts
@@ -0,0 +1,22 @@
+export class ClinicalDiagnosis {
+
+  //#region Atributes
+  private background: string;
+  private consoultationReason: string;
+  private symptoms: string;
+    private date: Date;
+
+  constructor(background: string = '', consultationReason: string= '', symptoms: string= '', date: Date= new Date())  {
+    this.background = background;
+    this.consoultationReason = consultationReason;
+    this.symptoms = symptoms;
+    this.date = date;
+  }
+  //#endregion
+
+  //#region Methods
+  //#endregion
+
+
+
+}
