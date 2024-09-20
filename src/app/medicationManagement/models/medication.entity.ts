58b704b7894812fb5dc9fa1708b16d3e6a1e4987
Valentino Sebastian Cervantes Erequita <u202110140@upc.edu.pe>
2024-09-20 18:40:31 -0500

    refactor: updating entity

--- src/app/medicationManagement/models/medication.entity.ts
+++ src/app/medicationManagement/models/medication.entity.ts
@@ -1,16 +1,19 @@
-
 export class Medication {
   name: string;
   description: string;
+  id: number;
+  startDate: Date;
+  endDate: Date;
+  interval: String;
   quantity: number;
-  medicalPrescription: string;
-  prescription: string;
 
   constructor() {
     this.name = '';
     this.description = '';
+    this.id = 0;
+    this.startDate = new Date();
+    this.endDate = new Date();
+    this.interval= '';
     this.quantity = 0;
-    this.medicalPrescription = '';
-    this.prescription = '';
   }
 }
--- src/app/medicationManagement/models/prescription.entity.ts
+++ src/app/medicationManagement/models/prescription.entity.ts
@@ -1,21 +1,10 @@
-import { Medication } from './medication.entity';
-
 export class Prescription {
-  name: string;
+  id: number;
+  medications: number[];
   description: string;
-  startDate: Date;
-  endDate: Date;
-  createdAt: Date;
-  updatedAt: Date;
-  medications: Medication[];
-
-  constructor(endDate: Date) {
-    this.name = '';
-    this.description = '';
-    this.startDate = new Date();
-    this.endDate = endDate;
-    this.createdAt = new Date();
-    this.updatedAt = new Date();
+  constructor() {
     this.medications = [];
+    this.description = '';
+    this.id = 0;
   }
 }
