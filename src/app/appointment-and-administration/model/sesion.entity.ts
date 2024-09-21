cad5a09be21a46783ba4e9a26d3e495853b1b83d
Anatoly69420 <u202211813@upc.edu.pe>
2024-09-20 21:43:28 -0500

    feat(appointment-and-administration): add session service to get the endpoint of sessions

--- src/app/appointment-and-administration/model/sesion.entity.ts
+++ src/app/appointment-and-administration/model/sesion.entity.ts
@@ -1,55 +1,30 @@
 export class Session {
-  //#region Attributes
-  private readonly id: number;
-  private appointmentDate: Date;
-  private sessionTime: number;
-  private readonly patientId: number;
-  private readonly professionalId: number;
-  private readonly noteId: number;
-  private createdAt: Date;
-  private updatedAt: Date;
-  //#endregion
+  id: number;
+  idProfessional: number;
+  idPatient: number;
+  idNote: number;
+  appointmentDate: string;
+  sessionTime: number;
+  createdAt: string;
+  updatedAt: string;
 
-  //#region Constructor
-  constructor(
-    id: number = 0,
-    appointmentDate: Date = new Date(),
-    sessionTime: number = 0,
-    patientId: number = 0,
-    professionalId: number = 0,
-    noteId: number = 0,
-    createdAt: Date = new Date(),
-    updatedAt: Date = new Date()
-  ) {
-    this.id = id;
-    this.appointmentDate = appointmentDate;
-    this.sessionTime = sessionTime;
-    this.patientId = patientId;
-    this.professionalId = professionalId;
-    this.noteId = noteId;
-    this.createdAt = createdAt;
-    this.updatedAt = updatedAt;
+  constructor(session: {
+    id?: number,
+    idProfessional?: number,
+    idPatient?: number,
+    idNote?: number,
+    appointmentDate?: string,
+    sessionTime?: number,
+    createdAt?: string,
+    updatedAt?: string
+  }) {
+    this.id = session.id || 0;
+    this.idProfessional = session.idProfessional || 0;
+    this.idPatient = session.idPatient || 0;
+    this.idNote = session.idNote || 0;
+    this.appointmentDate = session.appointmentDate || '';
+    this.sessionTime = session.sessionTime || 0;
+    this.createdAt = session.createdAt || '';
+    this.updatedAt = session.updatedAt || '';
   }
-  //#endregion
-
-  //#region Methods
-  /**
-   * Get session details.
-   * @returns {string} - A string with session details.
-   */
-  public getSessionDetails(): string {
-    return `Session ID: ${this.id}, Appointment Date: ${this.appointmentDate.toISOString()}, Session Time: ${this.sessionTime} hours, Patient ID: ${this.patientId}, Professional ID: ${this.professionalId}, Note ID: ${this.noteId}.`;
-  }
-
-  /**
-   * Update the session details.
-   * @param appointmentDate - New appointment date.
-   * @param sessionTime - New session time in hours.
-   */
-  public updateSession(appointmentDate: Date, sessionTime: number): void {
-    this.appointmentDate = appointmentDate;
-    this.sessionTime = sessionTime;
-    this.updatedAt = new Date();
-  }
-  //#endregion
 }
---
+++ src/app/appointment-and-administration/services/session.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import {BaseService} from "../../shared/services/base.service";
+import {Session} from "../model/sesion.entity";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SessionService extends BaseService<Session> {
+
+  constructor() {
+    super();
+    this.resourceEndpoint = '/sessions';
+  }
+}
