d6a776fb2ac3cfa6e2390fc63aef01546e96c31e
JanielFranz <102563480+JanielFranz@users.noreply.github.com>
2024-09-20 19:02:51 -0500

    fix: patient entity and and user fixed

--- src/app/shared/model/patient.entity.ts
+++ src/app/shared/model/patient.entity.ts
@@ -1,8 +1,12 @@
 import {User} from "./user.entity";
 
-export class Patient extends User{
-  constructor() {
-    super();
+export class Patient extends User {
+  private idClinicalHistory: number;
 
+  constructor(id: number, name: string,
+              lastName: string, email:string,
+              idAccount: number, idClinicalHistory : number) {
+    super(id, name, lastName, email, idAccount);
+    this.idClinicalHistory = idClinicalHistory;
   }
 }
