2be79e33fcadfc5dc6958f5d13ce1299ebb8bee3
JanielFranz <102563480+JanielFranz@users.noreply.github.com>
2024-09-21 01:21:56 -0500

    fix(patient): entity received object as a parameter

--- src/app/shared/model/patient.entity.ts
+++ src/app/shared/model/patient.entity.ts
@@ -3,10 +3,11 @@ import {User} from "./user.entity";
 export class Patient extends User {
   private idClinicalHistory: number;
 
-  constructor(id: number, name: string,
-              lastName: string, email:string,
-              idAccount: number, idClinicalHistory : number) {
-    super(id, name, lastName, email, idAccount);
-    this.idClinicalHistory = idClinicalHistory;
+  constructor(patient: {id: number, name: string,
+    lastName: string, email:string,
+    idAccount: number, idClinicalHistory : number}) {
+    super(patient.id, patient.name, patient.lastName, patient.email,
+      patient.idAccount);
+    this.idClinicalHistory = patient.idClinicalHistory;
   }
 }
