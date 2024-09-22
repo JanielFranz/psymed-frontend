0b742fa9f7e1067d771863aee42a796b0ab2ee96
Valentino Sebastian Cervantes Erequita <u202110140@upc.edu.pe>
2024-09-22 14:45:24 -0500

    chore: adding medication-form.component.ts route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import {HomeComponent} from "./public/pages/home/home.component";
+import {MedicationFormComponent} from "./medicationManagement/modules/medication-form/medication-form.component";
+
 
 export const routes: Routes = [
   {path: 'home', component: HomeComponent},
-
+  {path: 'medication-form', component: MedicationFormComponent},
 ];
