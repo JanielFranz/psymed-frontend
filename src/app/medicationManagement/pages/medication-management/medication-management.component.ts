db8b0b237692415eb060b9f72fcd23a1e8ed6c3d
Valentino Sebastian Cervantes Erequita <u202110140@upc.edu.pe>
2024-09-22 16:27:16 -0500

    feat: adding medication-form routing

--- src/app/medicationManagement/pages/medication-management/medication-management.component.ts
+++ src/app/medicationManagement/pages/medication-management/medication-management.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { MedicationFormComponent} from "../../modules/medication-form/medication-form.component";
 
 @Component({
   selector: 'app-medication-management',
   standalone: true,
-  imports: [],
+  imports: [MedicationFormComponent],
   templateUrl: './medication-management.component.html',
   styleUrl: './medication-management.component.css'
 })
 export class MedicationManagementComponent {
 
 }
+
