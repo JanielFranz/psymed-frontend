e5e0dd72f36d9f7fddb254c9aff0337c0645632e
Valentino Sebastian Cervantes Erequita <u202110140@upc.edu.pe>
2024-09-22 16:27:48 -0500

    refactor: updating medication-management path

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
 import {HomeComponent} from "./public/pages/home/home.component";
-import {MedicationFormComponent} from "./medicationManagement/modules/medication-form/medication-form.component";
+import {MedicationManagementComponent} from "./medicationManagement/pages/medication-management/medication-management.component";
 
 
 export const routes: Routes = [
   {path: 'home', component: HomeComponent},
-  {path: 'medication-form', component: MedicationFormComponent},
+  {path: 'medication-management', component: MedicationManagementComponent},
 ];
