d15a8c5eab4e278174173ac6f81e05f2e15e0716
Valentino Sebastian Cervantes Erequita <u202110140@upc.edu.pe>
2024-09-22 14:46:08 -0500

    feat: adding medication-form logic

--- src/app/medicationManagement/modules/medication-form/medication-form.component.ts
+++ src/app/medicationManagement/modules/medication-form/medication-form.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Medication } from '../../models/medication.entity';
+import { MedicationService } from '../../services/medication.service';
 
 @Component({
   selector: 'app-medication-form',
   standalone: true,
   imports: [],
   templateUrl: './medication-form.component.html',
-  styleUrl: './medication-form.component.css'
+  styleUrls: ['./medication-form.component.css']  // Corrected styleUrls typo
 })
-export class MedicationFormComponent {
+export class MedicationFormComponent implements OnInit {
+  protected medicationData!: Medication;
 
+  constructor(private medicationService: MedicationService) {}
+
+  private getMedicationById(id: number) {
+    console.log("Fetching medication data...");
+    this.medicationService.getById(id).subscribe({
+      next: (response: Medication) => {
+        this.medicationData = response;
+        console.log("Medication data retrieved:", this.medicationData.name);
+      },
+      error: (error) => {
+        console.error("Failed to fetch medication data:", error);
+      },
+      complete: () => {
+        console.log("Medication data fetch completed");
+      }
+    });
+  }
+
+
+  ngOnInit(): void {
+    console.log("on init");
+    this.getMedicationById(1);
+  }
 }
