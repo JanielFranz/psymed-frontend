a300c2ce068e9795f387086ee3069e220bd2f269
Valentino Sebastian Cervantes Erequita <u202110140@upc.edu.pe>
2024-09-21 20:31:44 -0500

    feat: adding bounded context's components and pages

---
+++ src/app/medicationManagement/modules/medication-form/medication-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-medication-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './medication-form.component.html',
+  styleUrl: './medication-form.component.css'
+})
+export class MedicationFormComponent {
+
+}
---
+++ src/app/medicationManagement/pages/medication-management/medication-management.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-medication-management',
+  standalone: true,
+  imports: [],
+  templateUrl: './medication-management.component.html',
+  styleUrl: './medication-management.component.css'
+})
+export class MedicationManagementComponent {
+
+}
