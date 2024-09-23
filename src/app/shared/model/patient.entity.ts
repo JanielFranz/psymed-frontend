a85c21e2d1236e318df116fecb91910fbfaadfae
Ro <u202213765@upc.edu.pe>
2024-09-23 16:01:19 -0500

    fix(patient): changed user constructor.

--- src/app/shared/model/patient.entity.ts
+++ src/app/shared/model/patient.entity.ts
@@ -1,15 +1,30 @@
 import {User} from "./user.entity";
 
 export class Patient extends User{
-  constructor(patient: {
-    id: number,
-    name: string,
-    lastName: string,
-    email: string,
-    idAccount: string,
-    idClinicalHistory: string,
-  }) {
-    super(patient.id, patient.name, patient.lastName, patient.email, patient.idAccount, patient.idClinicalHistory);
 
+  private name: string;
+  private lastName: string;
+  private email: string;
+  private idAccount?: string;
+  private idClinicalHistory: string;
+
+  constructor(patient: {
+    id: number;
+    userName: string;
+    password: string;
+    role: string;
+    name: string;
+    lastName: string;
+    email: string;
+    idAccount?: string;
+    idClinicalHistory: string
+  }) {
+
+    super(patient.id, patient.userName, patient.password, patient.role);
+    this.name = patient.name;
+    this.lastName = patient.lastName;
+    this.email = patient.email;
+    this.idAccount = patient.idAccount;
+    this.idClinicalHistory = patient.idClinicalHistory;
   }
 }
