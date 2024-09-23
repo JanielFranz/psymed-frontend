d8972942af5b5e449bc25f4500a8ee7c0233ef7a
JanielFranz <102563480+JanielFranz@users.noreply.github.com>
2024-09-23 00:23:55 -0500

    feat(dashboard): showed in the template

---
+++ src/app/dashboard-analytics/pages/analytics-dashboard/analytics-dashboard.component.ts
@@ -0,0 +1,84 @@
+import { Component } from '@angular/core';
+import {BaseChartDirective} from "ng2-charts";
+
+@Component({
+  selector: 'app-analytics-dashboard',
+  standalone: true,
+  imports: [
+    BaseChartDirective
+  ],
+  templateUrl: './analytics-dashboard.component.html',
+  styleUrl: './analytics-dashboard.component.css'
+})
+export class AnalyticsDashboardComponent {
+  public SystemName: string = "MF1";
+  firstCopy = false;
+
+  // data
+  public lineChartData: Array<number> = [ 1,8,49];
+
+  public labelMFL: Array<any> = [
+    { data: this.lineChartData,
+      label: this.SystemName
+    }
+  ];
+  // labels
+  public lineChartLabels: Array<any> = ["2018-01-29 10:00:00", "2018-01-29 10:27:00", "2018-01-29 10:28:00"];
+
+  constructor(  ) { }
+
+  public lineChartOptions: any = {
+    responsive: true,
+    scales : {
+      yAxes: [{
+        ticks: {
+          max : 60,
+          min : 0,
+        }
+      }],
+      xAxes: [{
+
+
+      }],
+    },
+    plugins: {
+      datalabels: {
+        display: true,
+        align: 'top',
+        anchor: 'end',
+        //color: "#2756B3",
+        color: "#222",
+
+        font: {
+          family: 'FontAwesome',
+          size: 14
+        },
+
+      },
+      deferred: false
+
+    },
+
+  };
+
+  _lineChartColors:Array<any> = [{
+    backgroundColor: 'red',
+    borderColor: 'red',
+    pointBackgroundColor: 'red',
+    pointBorderColor: 'red',
+    pointHoverBackgroundColor: 'red',
+    pointHoverBorderColor: 'red'
+  }];
+
+
+
+  public ChartType = 'bar';
+
+  public chartClicked(e: any): void {
+    console.log(e);
+  }
+  public chartHovered(e: any): void {
+    console.log(e);
+  }
+
+}
