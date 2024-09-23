b71c699519f64837735a946d93d5d099f851dc0c
JanielFranz <102563480+JanielFranz@users.noreply.github.com>
2024-09-23 00:23:21 -0500

    feat(dashboard): showed in the template

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,10 @@
 import { Routes } from '@angular/router';
 import {HomeComponent} from "./public/pages/home/home.component";
 import {
-  DashboardAndAnalyticsComponent
-} from "./dashboard-analytics/pages/dashboard-and-analytics/dashboard-and-analytics.component";
+  AnalyticsDashboardComponent
+} from "./dashboard-analytics/pages/analytics-dashboard/analytics-dashboard.component";
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
-  { path: 'dashboard-analytics', component: DashboardAndAnalyticsComponent }
+  { path: 'dashboard-analytics', component: AnalyticsDashboardComponent }
 ];
