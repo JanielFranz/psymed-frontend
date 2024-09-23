3f1685ff924d86c5f3d5c206f79e80661da69d5e
Ro <u202213765@upc.edu.pe>
2024-09-23 16:22:25 -0500

    fix(patient-list): added patient list component

---
+++ src/app/user-mamagement/components/patient-list/patient-list.component.ts
@@ -0,0 +1,42 @@
+import {AfterViewInit, Component, inject, OnInit, ViewChild} from '@angular/core';
+import {Patient} from "../../../shared/model/patient.entity";
+import {PatientService} from "../../../shared/services/patient.service";
+import {MatPaginator} from "@angular/material/paginator";
+import {MatSort} from "@angular/material/sort";
+import {MatTableDataSource} from "@angular/material/table";
+
+@Component({
+  selector: 'app-patient-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './patient-list.component.html',
+  styleUrl: './patient-list.component.css'
+})
+export class PatientListComponent implements OnInit,AfterViewInit{
+
+  //#region Attributes
+  protected patientData!: Patient;
+  protected columnsToDisplay: string[] = ['id', 'name', 'lastName'];
+  @ViewChild(MatPaginator, {static: false})
+  protected paginator!: MatPaginator;
+  @ViewChild(MatSort, {static: false})
+  protected sort!: MatSort;
+  protected editMode: boolean = false;
+  protected dataSource!: MatTableDataSource<any>;
+  private patientService: PatientService = inject(PatientService);
+
+  constructor() {
+    this.editMode = false;
+    this.patientData = new Patient({});
+    this.dataSource = new MatTableDataSource();
+    console.log(this.patientData);
+  }
+
+  ngAfterViewInit() {
+  }
+
+  ngOnInit() {
+  }
+
+
+}
