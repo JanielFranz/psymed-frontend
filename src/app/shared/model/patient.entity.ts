d99c9e262e17ae862989dc18245b9b728f094f32
Ro <u202213765@upc.edu.pe>
2024-09-23 15:03:14 -0500

    feat(patient): added new characteristics to the constructor and super.

--- src/app/shared/model/patient.entity.ts
+++ src/app/shared/model/patient.entity.ts
@@ -1,8 +1,15 @@
 import {User} from "./user.entity";
 
 export class Patient extends User{
-  constructor() {
-    super();
+  constructor(patient: {
+    id: number,
+    name: string,
+    lastName: string,
+    email: string,
+    idAccount: string,
+    idClinicalHistory: string,
+  }) {
+    super(patient.id, patient.name, patient.lastName, patient.email, patient.idAccount, patient.idClinicalHistory);
 
   }
 }
