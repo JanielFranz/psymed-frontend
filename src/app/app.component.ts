0f4ad782db3542a9580dddd97c0524455bb07596
JanielFranz <102563480+JanielFranz@users.noreply.github.com>
2024-09-23 17:46:36 -0500

    feat(analytic-page): first version of toolbar added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import {RouterLink, RouterOutlet} from '@angular/router';
+import {MatToolbar} from "@angular/material/toolbar";
+import {MatAnchor, MatIconButton} from "@angular/material/button";
+import {MatMenu, MatMenuTrigger} from "@angular/material/menu";
+import {MatIcon} from "@angular/material/icon";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, MatToolbar, MatAnchor, MatIconButton, MatMenuTrigger, MatIcon, MatMenu],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -12,6 +16,6 @@ export class AppComponent {
   title = 'psymed-frontend';
   protected options = [
     { path: '/home', title: 'Home' },
-    { path: '/learning/courses', title: 'Course' }
+    { path: '/dashboard-analytics', title: 'Dashboard' }
   ]
 }
