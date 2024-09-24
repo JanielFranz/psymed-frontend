ee0768ea4d2add776965014a12800701907c34d8
JanielFranz <102563480+JanielFranz@users.noreply.github.com>
2024-09-24 13:51:11 -0500

    feat(analytic-dashboard): onInit added to pie chart

--- src/app/dashboard-analytics/components/pie-chart/pie-chart.component.ts
+++ src/app/dashboard-analytics/components/pie-chart/pie-chart.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnChanges, SimpleChanges} from '@angular/core';
+import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
 import {MoodAnalytic} from "../../model/mood-analytic.entity";
 import {ChartType, ChartOptions, ChartData} from "chart.js";
 import {BaseChartDirective} from "ng2-charts";
@@ -13,7 +13,7 @@ import {BaseChartDirective} from "ng2-charts";
   templateUrl: './pie-chart.component.html',
   styleUrl: './pie-chart.component.css'
 })
-export class PieChartComponent implements OnChanges {
+export class PieChartComponent implements OnChanges, OnInit {
 // #region Attributes
   @Input() moodAnalytics!: MoodAnalytic;
   pieChartData!: ChartData<ChartType, string[], string>
@@ -25,6 +25,10 @@ export class PieChartComponent implements OnChanges {
 // #endregion
 
   // #region Methods
+  ngOnInit(): void {
+    this.initialData();
+  }
+
   ngOnChanges(changes: SimpleChanges): void {
     if(changes['moodAnalytics']){
       this.updateChart();
@@ -46,6 +50,24 @@ export class PieChartComponent implements OnChanges {
         }
       ]
     }
+  }
+
+  private initialData() {
+    this.pieChartData = {
+      labels: this.pieChartLabels,
+      datasets: [
+        {
+          data: ['0',
+            '0',
+            '0',
+            '0',
+            '0'
+          ],
+          backgroundColor: ['purple', '#36A2EB', 'grey', 'blue', '#FFCE56']
+        }
+      ]
+    }
+
   }
 
 
