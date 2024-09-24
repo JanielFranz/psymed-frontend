9afb7505ec17742d801dde1fecf3ff809f0c5b34
JanielFranz <102563480+JanielFranz@users.noreply.github.com>
2024-09-24 13:52:33 -0500

    feat(analytic-dashboard): service and functions added for biological functions

--- src/app/dashboard-analytics/pages/analytics-dashboard/analytics-dashboard.component.ts
+++ src/app/dashboard-analytics/pages/analytics-dashboard/analytics-dashboard.component.ts
@@ -4,13 +4,17 @@ import {MoodAnalyticService} from "../../services/mood-analytic.service";
 import {PieChartComponent} from "../../components/pie-chart/pie-chart.component";
 import {PieFilterComponent} from "../../components/pie-filter/pie-filter.component";
 import {ActivatedRoute} from "@angular/router";
+import {BarChartComponent} from "../../components/bar-chart/bar-chart.component";
+import {BiologicalAnalytic} from "../../model/biological-analytic.entity";
+import {BiologicalAnalyticService} from "../../services/biological-analytic.service";
 
 @Component({
   selector: 'app-analytics-dashboard',
   standalone: true,
   imports: [
     PieChartComponent,
-    PieFilterComponent
+    PieFilterComponent,
+    BarChartComponent
 
   ],
   templateUrl: './analytics-dashboard.component.html',
@@ -19,10 +23,12 @@ import {ActivatedRoute} from "@angular/router";
 export class AnalyticsDashboardComponent implements OnInit{
   // #region Attributes
   protected moodAnalyticData!: MoodAnalytic;
+  protected biologicalAnalyticData!: BiologicalAnalytic;
   private patientId!: string;
 
-  constructor(private moodAnalyticService: MoodAnalyticService, private route: ActivatedRoute) {
-    this.moodAnalyticData = new MoodAnalytic({});
+  constructor(private moodAnalyticService: MoodAnalyticService,
+              private route: ActivatedRoute,
+              private biologicalAnalyticService: BiologicalAnalyticService) {
   }
 
 
@@ -32,6 +38,7 @@ export class AnalyticsDashboardComponent implements OnInit{
   // #region Methods
   onFilterRequested(date: {month: any, year: any}){
     this.getMoodAnalyticDataByDate(date.month, date.year)
+    this.getBiologicalAnalyticDataByDate(date.month, date.year)
   }
   getMoodAnalyticDataByDate(month: string, year: string) {
     this.moodAnalyticService.findByDateAndPatientId(month, year, this.patientId).subscribe((response: MoodAnalytic | null) => {
@@ -44,9 +51,23 @@ export class AnalyticsDashboardComponent implements OnInit{
       console.log('El response', response)
     })
   }
+  getBiologicalAnalyticDataByDate(month: string, year: string){
+    this.biologicalAnalyticService.findByDateAndPatientId(month, year, this.patientId)
+      .subscribe((response: BiologicalAnalytic | null) => {
+        if(response) {
+          this.biologicalAnalyticData = response;
+        }
+        else {
+          console.log("No data found")
+        }
+        console.log("El response", response)
+      })
+  }
 
   ngOnInit(): void {
     this.patientId = this.route.snapshot.paramMap.get('patientId')!;
+    this.getMoodAnalyticDataByDate('0', '0')
+    this.getBiologicalAnalyticDataByDate('0', '0')
     console.log("Start")
   }
   // #endregion Methods
