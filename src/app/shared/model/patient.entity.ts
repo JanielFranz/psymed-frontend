38ca0de49a43cf6fb4e6be5373658691f3d87e0d
RominaMaita <u202213765@upc.edu.pe>
2024-09-23 23:42:19 -0500

    fix: patient attributes.

--- src/app/shared/model/patient.entity.ts
+++ src/app/shared/model/patient.entity.ts
@@ -3,8 +3,8 @@ import { User } from './user.entity';
 export class Patient extends User {
   private idClinicalHistory: number;
 
-  constructor(patient: { id: number, name: string, lastName: string, email: string, idAccount: number, idClinicalHistory: number }) {
+  constructor(patient: { id?: number, name?: string, lastName?: string, email?: string, idAccount?: number, idClinicalHistory?: number }) {
     super(patient.id, patient.name, patient.lastName, patient.email, patient.idAccount);
-    this.idClinicalHistory = patient.idClinicalHistory;
+    this.idClinicalHistory = patient.idClinicalHistory || 0;
   }
 }
