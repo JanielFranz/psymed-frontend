4abe8d5deac71be52c366f1d1e6f7c0ccc76721c
Valentino Sebastian Cervantes Erequita <u202110140@upc.edu.pe>
2024-09-24 14:49:07 -0500

    fex: fixing merge's errors

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -24,8 +24,8 @@ export const routes: Routes = [
   { path: 'login'                   , component: LoginComponent },
   { path: 'patient-management'      , component: PatientManagementComponent },
   { path: 'medication-management/:patientId', component: MedicationManagementComponent },// we use the :patientId to pass the patient ID as a parameter
-  { path: 'mood-state/:patientId', component: MoodStatementEntryComponent }
-  { path: 'patient-appointment-list/:id', component: PatientAppointmentPageComponent } // Updated :id to :patientId
+  { path: 'mood-state/:patientId', component: MoodStatementEntryComponent },
+  { path: 'patient-appointment-list/:id', component: PatientAppointmentPageComponent }, // Updated :id to :patientId
   { path: 'clinical-history/:historyId', component: HistoryManagementComponent },
   { path: 'biological-functions/:patientId', component: BiologicalFunctionsEntryComponent }
 ];
