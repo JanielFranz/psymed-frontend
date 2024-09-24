82dd72f4cfc05990067dfb2ab3bd6af74284a88c
Valentino Sebastian Cervantes Erequita <u202110140@upc.edu.pe>
2024-09-23 20:12:09 -0500

    refactor: updating patient entity's structure

--- src/app/shared/model/patient.entity.spec.ts
+++ src/app/shared/model/patient.entity.spec.ts
@@ -2,6 +2,14 @@ import { Patient } from './patient.entity';
 
 describe('PatientEntity', () => {
   it('should create an instance', () => {
-    expect(new Patient()).toBeTruthy();
+    const patientData = {
+      id: 1,
+      name: 'Callapin',
+      lastName: 'bombin',
+      email: 'callampa.mal@example.com',
+      idAccount: 123,
+      idClinicalHistory: 456
+    };
+    expect(new Patient(patientData)).toBeTruthy();
   });
 });
