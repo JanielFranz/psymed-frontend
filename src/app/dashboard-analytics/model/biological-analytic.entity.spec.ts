d40eb1f0200bb2a414a2e433dcb68afcc89f4111
JanielFranz <102563480+JanielFranz@users.noreply.github.com>
2024-09-24 13:41:59 -0500

    feat(db): biological analytic entity added

---
+++ src/app/dashboard-analytics/model/biological-analytic.entity.spec.ts
@@ -0,0 +1,7 @@
+import { BiologicalAnalyticEntity } from './biological-analytic.entity';
+
+describe('BiologicalAnalyticEntity', () => {
+  it('should create an instance', () => {
+    expect(new BiologicalAnalyticEntity()).toBeTruthy();
+  });
+});
