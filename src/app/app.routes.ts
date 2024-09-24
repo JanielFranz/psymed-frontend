f957d34bfaf040faddaf2f24ed22d2816e7b8c7f
Valentino Sebastian Cervantes Erequita <u202110140@upc.edu.pe>
2024-09-24 14:34:49 -0500

    feat
    Adding biological functions's route
    
    Co-Authored-By: Eduardo Sihuar Ccotarma Ttito <116849769+Anx0123@users.noreply.github.com>

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,6 +8,7 @@ import { MedicationManagementComponent} from "./medicationManagement/pages/medic
 import {PatientManagementComponent} from "./user-mamagement/pages/patient-management/patient-management.component";
 import {MoodStatementEntryComponent} from "./patientEntrys/pages/mood-statement-entry/mood-statement-entry.component";
 import {HistoryManagementComponent} from "./user-mamagement/pages/history-management/history-management.component";
+import  {BiologicalFunctionsEntryComponent} from "./patientEntrys/pages/biological-functions-entry/biological-functions-entry.component";
 
 export const routes: Routes = [
   { path: 'home'                    , component: HomeComponent },
@@ -17,5 +18,6 @@ export const routes: Routes = [
   { path: 'patient-management'      , component: PatientManagementComponent },
   { path: 'medication-management/:patientId', component: MedicationManagementComponent },// we use the :patientId to pass the patient ID as a parameter
   { path: 'mood-state/:patientId', component: MoodStatementEntryComponent },
-  { path: 'clinical-history/:historyId', component: HistoryManagementComponent }
+  { path: 'clinical-history/:historyId', component: HistoryManagementComponent },
+  { path: 'biological-functions/:patientId', component: BiologicalFunctionsEntryComponent }
 ];
