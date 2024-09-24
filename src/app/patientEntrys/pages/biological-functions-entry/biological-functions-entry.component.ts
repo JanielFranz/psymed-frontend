12212f9de02a480edf2653e7cbef30e441212d5c
Valentino Sebastian Cervantes Erequita <u202110140@upc.edu.pe>
2024-09-24 14:37:30 -0500

    feat
    Implement biological functions's components
    
    Co-Authored-By: Eduardo Sihuar Ccotarma Ttito <116849769+Anx0123@users.noreply.github.com>

--- src/app/patientEntrys/pages/biological-functions-entry/biological-functions-entry.component.ts
+++ src/app/patientEntrys/pages/biological-functions-entry/biological-functions-entry.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import {BiologicalFormComponent} from "../../components/biological-form/biological-form.component";
 
 @Component({
   selector: 'app-biological-functions-entry',
   standalone: true,
-  imports: [],
+  imports: [BiologicalFormComponent],
   templateUrl: './biological-functions-entry.component.html',
   styleUrl: './biological-functions-entry.component.css'
 })
