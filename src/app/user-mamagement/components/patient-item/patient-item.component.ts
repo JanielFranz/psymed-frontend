8064c24f68772f7eda3e7586e10243bb6d854085
JanielFranz <102563480+JanielFranz@users.noreply.github.com>
2024-09-24 01:59:40 -0500

    feat(patient-management): buttons and styles added to patient item

--- src/app/user-mamagement/components/patient-item/patient-item.component.ts
+++ src/app/user-mamagement/components/patient-item/patient-item.component.ts
@@ -1,13 +1,18 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {Patient} from "../../../shared/model/patient.entity";
-import {MatListItem, MatListItemAvatar} from "@angular/material/list";
+import {MatListItem, MatListItemAvatar, MatListItemLine} from "@angular/material/list";
+import {MatIcon} from "@angular/material/icon";
+import {MatIconButton} from "@angular/material/button";
 
 @Component({
   selector: 'app-patient-item',
   standalone: true,
   imports: [
     MatListItem,
-    MatListItemAvatar
+    MatListItemAvatar,
+    MatIcon,
+    MatIconButton,
+    MatListItemLine
   ],
   templateUrl: './patient-item.component.html',
   styleUrl: './patient-item.component.css'
