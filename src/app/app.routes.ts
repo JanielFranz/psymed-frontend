6db8a52203729cb33a015c14618f4f6887f91910
RominaMaita <u202213765@upc.edu.pe>
2024-09-23 23:00:10 -0500

    chore: added app.routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,10 +5,12 @@ import {
 } from "./dashboard-analytics/pages/analytics-dashboard/analytics-dashboard.component";
 
 import { MedicationManagementComponent} from "./medicationManagement/pages/medication-management/medication-management.component";
+import {PatientManagementComponent} from "./user-mamagement/pages/patient-management/patient-management.component";
 
 export const routes: Routes = [
   { path: 'home'                    , component: HomeComponent },
   { path: 'dashboard-analytics'      , component: AnalyticsDashboardComponent },
-  { path: 'medication-management', component: MedicationManagementComponent }
+  { path: 'medication-management', component: MedicationManagementComponent },
+  { path: 'patient-management', component: PatientManagementComponent },
 
 ];
