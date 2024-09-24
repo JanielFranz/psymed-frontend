8916f04d32f20d85addc80c76a575885a2e49451
Valentino Sebastian Cervantes Erequita <u202110140@upc.edu.pe>
2024-09-24 02:02:26 -0500

    refactor: updating Task's component' route

--- src/app/taskManagement/modules/task-form/task-form.component.ts
+++ src/app/taskManagement/modules/task-form/task-form.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { Task } from '../../models/task.entity';
 import { TaskServiceService } from '../../services/task.service.service';
 import { OnInit} from "@angular/core";
-
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-task-form',
@@ -12,6 +12,12 @@ import { OnInit} from "@angular/core";
   styleUrl: './task-form.component.css'
 })
 export class TaskFormComponent implements OnInit {
+
+  constructor(
+    private taskService: TaskServiceService,
+    private route: ActivatedRoute
+  ) {
+  }
 
 
   ngOnInit() {
