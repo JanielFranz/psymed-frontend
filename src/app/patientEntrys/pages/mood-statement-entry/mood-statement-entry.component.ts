2ed9546f72eaa78c4711a7a8c3a097cc2905dd2c
Valentino Sebastian Cervantes Erequita <u202110140@upc.edu.pe>
2024-09-24 02:33:47 -0500

    refactor: updating component's work

--- src/app/patientEntrys/pages/mood-statement-entry/mood-statement-entry.component.ts
+++ src/app/patientEntrys/pages/mood-statement-entry/mood-statement-entry.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {MoodState} from "../../models/mood-state.entity";
+import {MoodStateService} from "../../services/mood-state.service";
+
 
 @Component({
   selector: 'app-mood-statement-entry',
@@ -7,6 +11,16 @@ import { Component } from '@angular/core';
   templateUrl: './mood-statement-entry.component.html',
   styleUrl: './mood-statement-entry.component.css'
 })
-export class MoodStatementEntryComponent {
+export class MoodStatementEntryComponent implements OnInit{
+  patientId!: number;
+
+  constructor(private moodStateService: MoodStateService, private route: ActivatedRoute) {
+  }
+
+  ngOnInit() {
+    console.log("on init");
+    this.patientId = +this.route.snapshot.paramMap.get('patientId')!;
+    console.log("Patient ID:", this.patientId);
+  }
 
 }
