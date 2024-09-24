b26e22f2e43546e7bcf2e6f23cd3108e934cc8da
JanielFranz <102563480+JanielFranz@users.noreply.github.com>
2024-09-23 21:10:07 -0500

    feat(routes): login route added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,10 @@ import {HomeComponent} from "./public/pages/home/home.component";
 import {
   AnalyticsDashboardComponent
 } from "./dashboard-analytics/pages/analytics-dashboard/analytics-dashboard.component";
+import {LoginComponent} from "./public/pages/login/login.component";
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
-  { path: 'dashboard-analytics', component: AnalyticsDashboardComponent }
+  { path: 'dashboard-analytics', component: AnalyticsDashboardComponent },
+  { path: 'login', component: LoginComponent }
 ];
