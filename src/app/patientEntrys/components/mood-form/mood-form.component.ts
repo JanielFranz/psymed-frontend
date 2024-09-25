29784c47c8eefd7171eb0de2cebbf80a902fb0eb
JanielFranz <102563480+JanielFranz@users.noreply.github.com>
2024-09-25 00:44:31 -0500

    feat(mood-form): mood form working with the id from the store

--- src/app/patientEntrys/components/mood-form/mood-form.component.ts
+++ src/app/patientEntrys/components/mood-form/mood-form.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute} from "@angular/router";
 import {MoodState} from "../../models/mood-state.entity";
 import {MoodStateService} from "../../services/mood-state.service";
+import {Store} from "@ngrx/store";
+import {AuthState} from "../../../store/auth/auth.state";
+import {map, Observable} from "rxjs";
+import {selectPatientId} from "../../../store/auth/auth.selectors";
 
 @Component({
   selector: 'app-mood-form',
@@ -11,29 +14,47 @@ import {MoodStateService} from "../../services/mood-state.service";
   styleUrl: './mood-form.component.css'
 })
 export class MoodFormComponent implements OnInit {
-  patientId!: number; // Define the patient ID as a number
+  patientId!: number | null; // Define the patient ID as a number
   currentDate: string = "hoy xd";
+  patientId$!: Observable<number | null>;
 
-  constructor(private moodStateService: MoodStateService, private route: ActivatedRoute) {
+
+  constructor(private moodStateService: MoodStateService,
+              private store: Store<AuthState>) {
   }
   ngOnInit(): void {
+
     console.log("on init");
-    this.patientId = +this.route.snapshot.paramMap.get('patientId')!; // Get the patient ID from the route using the ActivatedRoute service
-    console.log("Patient ID:", this.patientId); // Log the patient ID to the console for security
+    this.patientId$ = this.store.select(selectPatientId);
+    this.patientId$.subscribe(patientId => {
+      this.patientId = patientId;
+      console.log('Patient Id:', this.patientId);
+    })
+
+    // this.patientId = +this.route.snapshot.paramMap.get('patientId')!; // Get the patient ID from the route using the ActivatedRoute service
+    // console.log("Patient ID:", this.patientId); // Log the patient ID to the console for security
   }
+
 
   selectMood(mood: number) {
-    this.moodStateService.getMoodStatesByPatientId(this.patientId).subscribe(moodStates => {
-      const existingMood = moodStates.find(m => m.createdAt === this.currentDate);
-      if (existingMood) {
-        alert('You already have a mood recorded for today.');
-      } else {
-        const newMood = new MoodState(1, this.patientId, mood, this.currentDate);
-        this.moodStateService.createMoodState(newMood, this.patientId).subscribe(() => {
-          window.location.reload();
-        });
-      }
-    });
+    this.patientId$.pipe(
+      map((patientId) => {
+        if(patientId !== null) {
+          this.moodStateService.getMoodStatesByPatientId(patientId).subscribe(moodStates => {
+            const existingMood = moodStates.find(m => m.createdAt === this.currentDate);
+            if (existingMood) {
+              alert('You already have a mood recorded for today.');
+            } else {
+              const newMood = new MoodState(1, patientId, mood, this.currentDate);
+              this.moodStateService.createMoodState(newMood, patientId).subscribe(() => {
+                window.location.reload();
+              });
+            }
+          });
+        } else {
+          alert('Patient ID unavailable');
+        }
+      })
+    ).subscribe();
   }
-
 }
