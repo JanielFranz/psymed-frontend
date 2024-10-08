3d0ab4151d83c8b27b4f9c719bf85cae4fbd1eb4
Anatoly69420 <u202211813@upc.edu.pe>
2024-10-08 18:18:42 -0500

    docs(login-component): add method documentation

--- src/app/public/pages/login/login.component.ts
+++ src/app/public/pages/login/login.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
-import {MatIconButton} from "@angular/material/button";
-import {MatIcon} from "@angular/material/icon";
-import {Store} from "@ngrx/store";
-import {setPatientId, setRole} from '../../../store/auth/auth.actions'
-import {MatCard, MatCardContent, MatCardTitle} from "@angular/material/card";
+import { MatButtonModule, MatIconButton } from "@angular/material/button";
+import { MatIcon } from "@angular/material/icon";
+import { Store } from "@ngrx/store";
+import { setPatientId, setRole } from '../../../store/auth/auth.actions';
+import { MatCard, MatCardContent, MatCardTitle } from "@angular/material/card";
+
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
+    MatButtonModule, // Ensure MatButtonModule is included for buttons
     MatIconButton,
     MatIcon,
     MatCardContent,
@@ -15,20 +17,28 @@ import {MatCard, MatCardContent, MatCardTitle} from "@angular/material/card";
     MatCardTitle
   ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
   constructor(private store: Store) {}
 
+  /**
+   * Dispatches professional role data to the store.
+   * @param {string} rolid - The role ID to set in the store.
+   */
   sendProfessionalDataToStore(rolid: string ): void {
-    // Dispatch the action to send rolid to the store
-    console.log(rolid)
+    console.log(rolid);
     this.store.dispatch(setRole({ rolid }));
   }
 
+  /**
+   * Dispatches patient role and patient ID to the store.
+   * @param {string} rolid - The role ID to set in the store.
+   * @param {number} patientId - The patient ID to set in the store.
+   */
   sendPatientDataToStore(rolid: string, patientId: number): void {
-    console.log(rolid)
-    console.log(patientId)
+    console.log(rolid);
+    console.log(patientId);
 
     this.store.dispatch(setRole({ rolid }));
     this.store.dispatch(setPatientId({ patientId }));
