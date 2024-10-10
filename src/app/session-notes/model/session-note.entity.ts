7f08798b0fe3382f7a748352aa9d111c132ede54
JanielFranz <102563480+JanielFranz@users.noreply.github.com>
2024-10-09 20:09:10 -0500

    feat(session-notes): new attributes added to session entity

--- src/app/session-notes/model/session-note.entity.ts
+++ src/app/session-notes/model/session-note.entity.ts
@@ -1,9 +1,13 @@
 export class SessionNote {
+  id: string;
+  patientId: string;
   title: string;
   description: string;
   date: string;
 
-  constructor(title: string, description: string, date: string) {
+  constructor(id: string, patientId: string, title: string, description: string, date: string) {
+    this.id = id;
+    this.patientId = patientId;
     this.title = title;
     this.description = description;
     this.date = date;
