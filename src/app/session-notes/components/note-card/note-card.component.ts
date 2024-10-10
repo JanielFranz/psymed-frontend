fdda908f30686b4168bbc553bfff23b30c9195d7
JanielFranz <102563480+JanielFranz@users.noreply.github.com>
2024-10-09 19:52:32 -0500

    feat: note card added

---
+++ src/app/session-notes/components/note-card/note-card.component.ts
@@ -0,0 +1,48 @@
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {MatDialog} from "@angular/material/dialog";
+import {SessionNote} from "../../model/session-note.entity";
+import {NoteDialogComponent} from "../note-dialog/note-dialog.component";
+import {MatDialogModule} from "@angular/material/dialog";
+import {MatCard, MatCardActions, MatCardContent, MatCardHeader, MatCardTitle} from "@angular/material/card";
+import {MatButton} from "@angular/material/button";
+
+@Component({
+  selector: 'app-note-card',
+  standalone: true,
+  imports: [MatDialogModule, MatCard, MatCardHeader, MatCardTitle, MatCardContent, MatCardActions, MatButton],
+  templateUrl: './note-card.component.html',
+  styleUrl: './note-card.component.css'
+})
+export class NoteCardComponent {
+  //#region Properties
+  @Input() sessionNote!: SessionNote;
+  @Output() deleteNoteRequested = new EventEmitter<SessionNote>();
+  constructor(private dialog: MatDialog) {}
+  //#endregion
+
+  //#region Utility Methods
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(NoteDialogComponent, {
+      width: '250px',
+    })
+
+    dialogRef.componentInstance.deleteNoteRequested.subscribe(() => {
+      this.onDelete();
+    })
+
+  }
+
+  //#endregion
+
+  //#region Event Handlers
+  protected onCancel() {
+    this.dialog.closeAll();
+  }
+
+  protected onDelete() {
+    this.deleteNoteRequested.emit(this.sessionNote)
+
+  }
+  //#endregion
+}
