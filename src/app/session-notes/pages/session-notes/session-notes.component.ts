8f00eeef1efc0340bc2e6b007c665592aabdcdcb
JanielFranz <102563480+JanielFranz@users.noreply.github.com>
2024-10-09 19:50:57 -0500

    feat: session note page added

---
+++ src/app/session-notes/pages/session-notes/session-notes.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-session-notes',
+  standalone: true,
+  imports: [],
+  templateUrl: './session-notes.component.html',
+  styleUrl: './session-notes.component.css'
+})
+export class SessionNotesComponent {
+
+}
