441cdb15a4b6e80aa8d96c0a67543c8663bad4b0
Valentino Sebastian Cervantes Erequita <u202110140@upc.edu.pe>
2024-10-11 20:23:10 -0500

    chore: adding task into sessions view

--- src/app/session-notes/pages/professional-view-task/professional-view-task.component.ts
+++ src/app/session-notes/pages/professional-view-task/professional-view-task.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {TaskListComponent} from "../../components/task-list/task-list.component";
+import {TaskFormComponent} from "../../components/task-form/task-form.component";
 
 @Component({
   selector: 'app-professional-view-task',
   standalone: true,
   imports: [
-    TaskListComponent
+    TaskListComponent,
+    TaskFormComponent
   ],
   templateUrl: './professional-view-task.component.html',
   styleUrl: './professional-view-task.component.css'
