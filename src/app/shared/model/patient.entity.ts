0f69a114497e17c1bf558a29cb8d836efd1a6f80
Anatoly69420 <u202211813@upc.edu.pe>
2024-10-11 23:42:57 -0500

    refactor: updated the user entity to have the new added attributes from the db.json and added documentation for a better understanding of the user entity

--- src/app/shared/model/patient.entity.ts
+++ src/app/shared/model/patient.entity.ts
@@ -1,7 +1,7 @@
 import { User } from './user.entity';
 
 export class Patient extends User {
-  private idClinicalHistory: number;
+  idClinicalHistory: number;
 
   constructor(patient: { id?: number, name?: string, lastName?: string, email?: string, idAccount?: number, idClinicalHistory?: number }) {
     super(patient.id, patient.name, patient.lastName, patient.email, patient.idAccount);
