073100d9705d5cc991eb7e08858d3da5e9d4829a
Valentino Sebastian Cervantes Erequita <u202110140@upc.edu.pe>
2024-10-11 20:24:01 -0500

    feat: adding task form functional component

---
+++ src/app/session-notes/components/task-form/task-form.component.ts
@@ -0,0 +1,66 @@
+import { Component, OnInit } from '@angular/core';
+import { Task} from "../../model/task.entity";
+import { TaskService } from "../../services/task.service";
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {ActivatedRoute} from "@angular/router";
+import {NgIf} from "@angular/common";
+
+@Component({
+  selector: 'app-task-form',
+  standalone: true,
+  imports: [
+    MatInputModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    NgIf
+  ],
+  templateUrl: './task-form.component.html',
+  styleUrl: './task-form.component.css'
+})
+export class TaskFormComponent implements OnInit{
+  taskForm!: FormGroup;
+  id!: number;
+  appointmentId!: number;
+  constructor(private fb: FormBuilder,
+              private taskService: TaskService,
+              private route: ActivatedRoute
+              ) {}
+
+  ngOnInit(): void {
+    this.id = +this.route.snapshot.paramMap.get('id')!;
+    this.appointmentId = +this.route.snapshot.paramMap.get('appointmentId')!;
+
+    this.taskForm = this.fb.group({
+      title: ['', Validators.required],
+      description: ['', Validators.required]
+    });
+    console.log("id de paciente al crear task", this.id);
+  }
+
+  onSubmit(): void {
+    if (this.taskForm.valid) {
+      const formValues = this.taskForm.value;
+      const newTask = new Task({
+        id: 0,
+        idPatient: this.id,
+        idSession: this.appointmentId,
+        title: formValues.title,
+        description: formValues.description,
+        status: 0,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      });
+
+      this.taskService.create(newTask).subscribe(response => {
+        console.log('Task created successfully', response);
+        window.location.reload();
+      });
+    }
+
+
+  }
+}
