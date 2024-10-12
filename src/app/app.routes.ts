7c1acf48a2bf0a67d5e3c136dd8f093817da6911
Anatoly69420 <u202211813@upc.edu.pe>
2024-10-12 16:16:31 -0500

    refactor: add changes in app routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,11 @@
+
 import { Routes } from '@angular/router';
 import {HomeComponent} from "./public/pages/home/home.component";
 import {
   AnalyticsDashboardComponent
 } from "./dashboard-analytics/pages/analytics-dashboard/analytics-dashboard.component";
 import {LoginComponent} from "./public/pages/login/login.component";
-import { MedicationManagementComponent} from "./medication-management/pages/medication-management/medication-management.component";
+import {MedicationManagementComponent} from "./medication-management/pages/medication-management/medication-management.component";
 import {PatientManagementComponent} from "./user-mamagement/pages/patient-management/patient-management.component";
 import {MoodStatementEntryComponent} from "./patientEntrys/pages/mood-statement-entry/mood-statement-entry.component";
 import {
@@ -12,14 +13,14 @@ import {
 } from "./appointment-and-administration/pages/patient-appointment-page/patient-appointment-page.component";
 import {HistoryManagementComponent} from "./user-mamagement/pages/history-management/history-management.component";
 import  {BiologicalFunctionsEntryComponent} from "./patientEntrys/pages/biological-functions-entry/biological-functions-entry.component";
-import {SessionNotesComponent} from "./session-notes/pages/session-notes/session-notes.component";
-import  {ProfessionalViewTaskComponent} from "./session-notes/pages/professional-view-task/professional-view-task.component";
 import {
   ProfessionalAppointmentPageComponent
 } from "./appointment-and-administration/pages/professional-appointment-page/professional-appointment-page.component";
-import {AccountProfileComponent} from "./iam/pages/account-profile/account-profile.component";
+import {SessionNotesComponent} from "./session-notes/pages/session-notes/session-notes.component";
+import  {ProfessionalViewTaskComponent} from "./session-notes/pages/professional-view-task/professional-view-task.component";
+import {PatientMedicationComponent} from "./medication-management/pages/patient-medication/patient-medication.component";
 import {EditProfileComponent} from "./iam/pages/edit-profile/edit-profile.component";
-
+import {AccountProfileComponent} from "./iam/pages/account-profile/account-profile.component";
 
 export const routes: Routes = [
   { path: ''                                                        , redirectTo: 'home', pathMatch: 'full' },  // Default route that redirects to HomeComponent
@@ -32,9 +33,11 @@ export const routes: Routes = [
   { path: 'patient-management/:id/patient-appointment-list'         , component: PatientAppointmentPageComponent },
   { path: 'patient-management/:id/clinical-history/:historyId'          , component: HistoryManagementComponent },
   { path: 'biological-functions'                                    , component: BiologicalFunctionsEntryComponent },
-  { path: 'appointment-list'                                        , component: ProfessionalAppointmentPageComponent },
+  { path: 'patient/prescription/:patientId', component: PatientMedicationComponent },
+  { path: 'appointment-list'                                        , component: ProfessionalAppointmentPageComponent},
   { path: 'patient-management/:id/patient-appointment-list/:appointmentId/notes'         , component: SessionNotesComponent },
-  { path: 'session-task'                                            , component: ProfessionalViewTaskComponent },
+  { path: 'patient-management/:id/patient-appointment-list/:appointmentId/task'         , component: ProfessionalViewTaskComponent },
+  { path: ':id/appointment-list/:appointmentId/task'  , component: ProfessionalViewTaskComponent },
   { path: 'profile'                                            , component: AccountProfileComponent },
   { path: 'edit-profile'                                            , component: EditProfileComponent },
   { path: '**'                                                      , redirectTo: 'home' }  // Wildcard route for invalid paths, redirects to HomeComponent
