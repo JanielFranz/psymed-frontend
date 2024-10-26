a7e604b352e5ee46ad3ad37d5e1157b4995ece80
Valentino Sebastian Cervantes Erequita <u202110140@upc.edu.pe>
2024-10-26 14:44:39 -0500

    chore: deleting useless component

--- src/app/user-mamagement/model/clinical-history.entity.spec.ts
+++
@@ -1,7 +0,0 @@
-import { ClinicalDiagnosisEntity } from './clinical-diagnosis.entity';
-
-describe('ClinicalDiagnosisEntity', () => {
-  it('should create an instance', () => {
-    expect(new ClinicalDiagnosisEntity()).toBeTruthy();
-  });
-});
