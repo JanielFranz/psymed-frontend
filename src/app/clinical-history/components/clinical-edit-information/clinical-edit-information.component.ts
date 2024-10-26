8a47d1ad927858ed7cc419b7e33317125985df69
Valentino Sebastian Cervantes Erequita <u202110140@upc.edu.pe>
2024-10-26 14:44:10 -0500

    feat: adding clinical History's update page for professional user

--- src/app/clinical-history/components/clinical-edit-information/clinical-edit-information.component.ts
+++ src/app/clinical-history/components/clinical-edit-information/clinical-edit-information.component.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ClinicalHistoryService} from "../../services/clinical-history.service";
+import {ClinicalHistory} from "../../models/clinical-history.entity";
+import {ActivatedRoute} from "@angular/router";
+import {FormsModule} from "@angular/forms";
+import {Location} from "@angular/common";
 
 @Component({
   selector: 'app-clinical-edit-information',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './clinical-edit-information.component.html',
   styleUrl: './clinical-edit-information.component.css'
 })
-export class ClinicalEditInformationComponent {
+export class ClinicalEditInformationComponent implements OnInit{
+  clinicalHistory!: ClinicalHistory;
 
+  constructor(
+    private route: ActivatedRoute,
+    private clinicalHistoryService: ClinicalHistoryService,
+    private location: Location
+  ) {}
+
+  ngOnInit(): void {
+    this.loadClinicalHistory();
+
+  }
+
+  loadClinicalHistory() {
+    const clinicalId = this.route.snapshot.params['clinicalHistoryId'];
+    this.clinicalHistoryService.getById(clinicalId).subscribe((clinicalHistory: ClinicalHistory) => {
+      this.clinicalHistory = clinicalHistory;
+    });
+    console.clear();
+  }
+
+  save() {
+    this.clinicalHistory.date = new Date().toLocaleDateString();
+    console.log("the id for edit clinical history:" + this.clinicalHistory.id)
+    this.clinicalHistoryService.updateClinicalHistory(this.clinicalHistory);
+    this.location.back();
+  }
 }
