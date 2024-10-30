19623b44815414097b65ce437a4a8d8b88f843dc
Valentino Sebastian Cervantes Erequita <u202110140@upc.edu.pe>
2024-10-30 12:33:01 -0500

    feat: adding reactive validation to task and notes adding.

---
+++ src/app/appointment-and-administration/pages/patient-apointment-view/patient-apointment-view.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-patient-apointment-view',
+  standalone: true,
+  imports: [],
+  templateUrl: './patient-apointment-view.component.html',
+  styleUrl: './patient-apointment-view.component.css'
+})
+export class PatientApointmentViewComponent {
+
+}
