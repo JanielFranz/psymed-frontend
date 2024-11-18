bf723f5ae3c83530a243a46bb86adc89941a5c1c
Valentino Sebastian Cervantes Erequita <u202110140@upc.edu.pe>
2024-11-18 06:25:10 -0500

    feat: adding service to user management

---
+++ src/app/user-mamagement/services/user-management.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import {BaseService} from "../../shared/services/base.service";
+import {PatientProfile} from "../../shared/model/patient-profile.entity";
+import {Store} from "@ngrx/store";
+import {catchError, Observable, retry} from "rxjs";
+import {HttpHeaders} from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserManagementService extends BaseService<PatientProfile>{
+
+  constructor() {
+    super();
+    this.resourceEndpoint = "/patient-profiles"
+
+  }
+
+  public createPatientProfile(patientProfile: PatientProfile, token: string): Observable<PatientProfile> {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+      })
+    };
+    return this.http.post<PatientProfile>(this.resourcePath(), JSON.stringify(patientProfile), httpOptions)
+      .pipe(retry(2), catchError(this.handleError));
+  }
+
+  public getPatientsByProfessionalId(professionalId: number, token: string): Observable<PatientProfile> {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+      })
+    };
+
+    return this.http.get<PatientProfile>(`${this.resourcePath()}/professional/${professionalId}`, httpOptions);
+  }
+
+
+
+}
